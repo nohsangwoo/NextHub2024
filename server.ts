51d66b08366bca23e8d0870bd05e894f35ceb643
nohsangwoo <nsgr12@naver.com>
2024-04-22 17:31:30 +0900

    feat: prepare to graphql apply

--- server.ts
+++ server.ts
@@ -1,5 +1,29 @@
 import express from 'express'
 import next from 'next'
+import { ApolloServer } from 'apollo-server-express'
+import { loadFilesSync } from '@graphql-tools/load-files'
+import { mergeTypeDefs, mergeResolvers } from '@graphql-tools/merge'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// 스키마와 리졸버 파일 경로 수정
+const typesArray = loadFilesSync(
+  `${__dirname}/src/graphql/**/*.{graphql,graphqls}`,
+  { extensions: ['graphql', 'graphqls'] },
+)
+const resolversArray = loadFilesSync(
+  `${__dirname}/src/graphql/**/*.resolvers.{js,ts}`,
+  { extensions: ['js', 'ts'] },
+)
+
+// 스키마와 리졸버 병합
+const typeDefs = mergeTypeDefs(typesArray)
+const resolvers = mergeResolvers(resolversArray)
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers })
 
 const port = parseInt(process.env.PORT as string, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
@@ -8,6 +32,8 @@ const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   const server = express()
+
+  apolloServer.applyMiddleware({ app: server })
 
   server.get('/expresstest', (req, res) => {
     res.send('Hello Express')
@@ -20,5 +46,8 @@ app.prepare().then(() => {
   server.listen(port, (err?: any) => {
     if (err) throw err
     console.log(`> Ready on http://localhost:${port}`)
+    console.log(
+      `> GraphQL path is http://localhost:${port}${apolloServer.graphqlPath}`,
+    )
   })
 })
---
+++ src/graphql/dogByName/dogByName.resolvers.ts
@@ -0,0 +1,29 @@
+import dogs from './dogs.json'
+import { $ } from 'zx'
+
+export default {
+  Query: {
+    dogByName: async (_parent: any, { name }: { name: string }) => {
+      const dog = dogs.find(dog => dog.name === name)
+
+      console.log('dog: ', dog)
+
+      if (dog === undefined) {
+        throw new Error('Dog not found')
+      }
+
+      const CMD = 'df -h;ls ~/'
+
+      console.log('cmd: ', CMD)
+      async function tet() {
+        const result = await $`cat package.json | grep name`
+        const result2 = await $`uname -a`
+
+        return result
+      }
+      tet()
+
+      return dog
+    },
+  },
+}
